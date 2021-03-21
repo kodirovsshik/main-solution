4055009cdc1828876d436b0445bbbabe157a751f
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-03-21 17:43:48 +0300

    [temp13]: Project emulates AVX512 instruction set under Intel SDE debugger. Project configured to use v142 tools and NASM as an assembler

--- temp13/temp13.nasm64.asm
+++ temp13/temp13.nasm64.asm
@@ -1,10 +1,27 @@
 
 global    main
 
+
+
+
 section   .text
-main:   
-	xor rax, rax
+
+
+main: nop
+	mov eax, 7
+	mov ecx, 0
+	cpuid 
+	test ebx, 0b10000000000000000
+	jnz .nret
+	mov eax, -1
+	ret
+.nret:
+
+	vpaddq zmm0, zmm1, zmm2
+	vpaddq zmm0, zmm1, zmm2
+	
+
+	xor eax, eax
 	ret
 
 section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
