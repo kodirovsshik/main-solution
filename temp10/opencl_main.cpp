e328170ac7bdeb92ae0727b4567dcbc989c71ac2
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-06-23 18:09:35 +0300

    Sync

--- temp10/opencl_main.cpp
+++ temp10/opencl_main.cpp
@@ -17,6 +17,8 @@
 //#include <ksn/opencl_selector.hpp>
 
 
+#pragma comment(lib, "Winmm.lib")
+
 //#pragma comment(lib, "OpenCL.lib")
 //#pragma comment(lib, "cfgmgr32.lib")
 //#pragma comment(lib, "runtimeobject.lib")
@@ -25,6 +27,7 @@
 #pragma comment(lib, "libksn_window_gl.lib")
 #pragma comment(lib, "libksn_stuff.lib")
 #pragma comment(lib, "libksn_x86_instruction_set.lib")
+#pragma comment(lib, "libksn_time.lib")
 #pragma comment(lib, "opengl32.lib")
 #pragma comment(lib, "glew32s.lib")
 
@@ -54,9 +57,25 @@
 #include <ksn/window.hpp>
 #include <ksn/window_gl.hpp>
 #include <ksn/stuff.hpp>
+#include <ksn/time.hpp>
 
 #include <GL/glew.h>
 
+void usleep(long long usec)
+{
+	static HANDLE timer = []
+	{
+		auto timer = CreateWaitableTimer(NULL, TRUE, NULL);   /* Timer handle */
+		if (!timer) abort();
+		return timer;
+	}();
+	static LARGE_INTEGER li;   /* Time defintion */
+
+	li.QuadPart = -usec * 10;
+
+	SetWaitableTimer(timer, &li, 0, NULL, NULL, FALSE);
+	WaitForSingleObject(timer, INFINITE);
+}
 
 
 int main()
@@ -64,16 +83,14 @@ int main()
 	static constexpr uint16_t width = 800;
 	static constexpr uint16_t height = 600;
 	static constexpr size_t screen_buffer_size = width * height * 3;
-	
+	static constexpr uint16_t framerate = 240;
 	
 	int ok;
 	(void)ok;
 	
-
-	
 	ksn::window_gl_t win;
-	win.open(width, height, "", ksn::window_gl_t::opengl_no_context);
-	win.context_create();
+	
+	win.open(width, height, "");
 	win.context_make_current();
 
 	ksn_dynamic_assert(win.is_open() && win.context_is_current(), "");
@@ -88,15 +105,28 @@ int main()
 	(void)color;
 
 	win.tick();
-	win.set_framerate(60);
+	win.set_framerate(framerate);
 
 	auto clock_f = std::chrono::high_resolution_clock::now;
 	auto t1 = clock_f();
 	
 	uint64_t fps_time_counter = 0;
-	int fps_time_counter_period = 10;
+	int fps_time_counter_period = framerate / 3;
+	int fps_avg_period = 10;
+	float fps_avg = 0;
 
-	size_t tick_counter = 0;
+	uint32_t tick_counter = 1;
+
+
+	using clock_t = std::chrono::high_resolution_clock;
+
+	auto time_now = clock_t::now();
+	auto time_last = clock_t::now();
+
+	ksn::stopwatch sw;
+	sw.start();
+
+	glClearColor(0, 0, 0, 0);
 
 	while (win.is_open())
 	{
@@ -106,18 +136,12 @@ int main()
 
 
 
+		glClear(GL_COLOR_BUFFER_BIT);
 		//Draw everything
 		win.draw_pixels_bgr_front(screen_buffer);
+		win.swap_buffers();
 		
-
-		//Limit framerate
-		win.tick();
-
-
-		//Measure frame time
-		auto t2 = clock_f();
-		fps_time_counter += std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
-
+		ksn::sleep_for(ksn::time::from_usec(1000000 / framerate) - sw.restart());
 
 		//Process events
 		ksn::event_t ev;
@@ -139,13 +163,30 @@ int main()
 			}
 		}
 
+		sw.restart();
+
+		
+
+		//Frame limiting
+		//time_now = clock_t::now();
+		//auto work_time = std::chrono::duration_cast<std::chrono::microseconds>(time_now - time_last).count();
+
+		//int64_t calculated_sleep_time = 1000000 / framerate - work_time;
+
+		////std::this_thread::sleep_for(std::chrono::microseconds(calculated_sleep_time));
+		//usleep(calculated_sleep_time);
+
+		//time_last = clock_t::now();
+		//auto sleep_time = duration_cast<std::chrono::microseconds>(time_last - time_now).count();
+
+
 
 		//Debug output
-		if ((tick_counter % fps_time_counter_period) == 0)
-		{
-			printf("FPS = %.1f\n", 1e9 / fps_time_counter * fps_time_counter_period);
-			fps_time_counter = 0;
-		}
+		//if ((tick_counter % fps_time_counter_period) == 0)
+		//{
+		//	printf("Work: %i, Sleep1: %i, Sleep2: %i, Cycle: %i, over: %i\n", (int)work_time, (int)sleep_time, (int)calculated_sleep_time, int(work_time + sleep_time), int(sleep_time - calculated_sleep_time));
+		//}
+		//if (tick_counter == framerate) __debugbreak();
 
 
 
