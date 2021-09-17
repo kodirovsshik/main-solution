e3a3b0d6d67d7c8e167e56b263d38faadd90349c
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-09-17 12:05:26 +0300

    Sync

--- temp14/Source.cpp
+++ temp14/Source.cpp
@@ -1,9 +1,47 @@
 
+#include <iostream>
+#include <vector>
 #include <algorithm>
+
+#include <stdio.h>
+
+#include <ksn/math_long_integer.hpp>
+
+
+#pragma warning(disable : 4996)
 
 int main()
 {
+	//(void)freopen("data.txt", "r", stdin);
+	//(void)freopen("answer.txt", "wb", stdout);
+
+	std::ios::sync_with_stdio(true);
+
+	using T = int;
+
+	T sum = 0;
+
+	size_t N;
+	std::cin >> N;
+	
+	std::vector<T> v;
+	v.reserve(N);
+
+	for (size_t i = 0; i < N; ++i)
+	{
+		T x;
+		std::cin >> x;
+
+		sum += x;
+		v.push_back(x);
+	}
 
 
 
+
+
+	fclose(stdout);
+	fclose(stdin);
+
+	return 0;
 }
