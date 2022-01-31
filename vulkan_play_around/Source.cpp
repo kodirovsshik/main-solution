2003b6ee2a7d66b6f868705fb0c3c66960993b59
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2022-01-31 08:50:51 +0300

    SPIR-V goes brrrr

--- vulkan_play_around/Source.cpp
+++ vulkan_play_around/Source.cpp
@@ -1,27 +1,25 @@
 ﻿
 #pragma warning(disable : 28251 26812 26495)
 
-#include <ksn/ksn.hpp>
 #include <ksn/window.hpp>
 #include <ksn/math_vec.hpp>
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <iostream>
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#include <unordered_set>
 #include <optional>
+#include <execution>
 
 
 #ifdef _WIN32
-#include <conio.h>
 #define VK_USE_PLATFORM_WIN32_KHR
-#include <vulkan/vulkan.hpp>
 #pragma comment(lib, "vulkan-1.lib")
 #endif
+
+#include <vulkan/vulkan.hpp>
 
 #pragma comment(lib, "libksn_window.lib")
 #pragma comment(lib, "libksn_time.lib")
@@ -46,16 +44,6 @@ const std::array vk_extension_data =
 };
 
 
-template<size_t N>
-constexpr auto get_name_lengthes_array(const std::array<const char*, N>& in)
-{
-	std::array<std::size_t, N> out;
-	for (size_t i = 0; i < N; ++i)
-		out[i] = std::char_traits<char>::length(in[i]);
-
-	return out;
-}
-
 const std::array vk_device_extensions =
 {
 	VK_KHR_SWAPCHAIN_EXTENSION_NAME,
@@ -66,7 +54,6 @@ const std::array vk_device_extensions =
 	//VK_KHR_SPIRV_1_4_EXTENSION_NAME,
 	VK_KHR_SHADER_NON_SEMANTIC_INFO_EXTENSION_NAME,
 };
-const std::array vk_device_extensions_name_lengthes = get_name_lengthes_array(vk_device_extensions);
 
 
 
@@ -147,32 +134,29 @@ class main_app_t
 	vk::Device vk_device;
 	vk::SurfaceKHR vk_surface;
 	vk::ShaderModule vk_test_shader;
+	vk::PhysicalDeviceMemoryProperties vk_device_memory_properties;
+
 
 	vk::Queue vk_compute_queue;
-	vk::Queue vk_transfer_queue;
-	vk::Queue vk_present_queue;
-
 	vk::CommandPool vk_compute_command_pool;
 	vk::CommandBuffer vk_compute_command_buffer;
+
+	vk::Queue vk_transfer_queue;
+	vk::CommandPool vk_transfer_command_pool;
+	vk::CommandBuffer vk_transfer_command_buffer;
+
+	vk::Queue vk_present_queue;
+	vk::CommandPool vk_present_command_pool;
+	vk::CommandBuffer vk_present_command_buffer;
+
 	vk::Pipeline vk_compute_pipeline;
 	vk::PipelineLayout vk_compute_pipeline_layout;
 	vk::DescriptorSetLayout vk_compute_pipeline_descriptor_set_layout;
 
+	std::vector<vk::DeviceMemory> m_vk_allocations;
+
 	void _init_create_instance()
 	{
-		//uint32_t v;
-		//auto x0 = vk::enumerateInstanceVersion(&v);
-		//auto x1 = vk::enumerateInstanceLayerProperties();
-		//for (const auto& x : x1)
-		//{
-		//	std::cout << x.layerName << ":\n\t" << x.description << "\n\n";
-		//}
-		//auto x2 = vk::enumerateInstanceExtensionProperties();
-		//for (const auto& x : x2)
-		//{
-		//	std::cout << x.extensionName << "\n";
-		//}
-
 		vk::InstanceCreateInfo create_info{};
 		create_info.ppEnabledLayerNames = vk_layers.data();
 		create_info.enabledLayerCount = (uint32_t)vk_layers.size();
@@ -191,23 +175,30 @@ class main_app_t
 		auto extensions = device.enumerateDeviceExtensionProperties();
 		std::vector<std::string_view> extension_names{ extensions.size() };
 
-		for (size_t i = 0; i < extensions.size(); ++i)
-			extension_names[i] = extensions[i].extensionName;
+		std::transform(std::execution::par_unseq, extensions.begin(), extensions.end(), extension_names.begin(), []
+		(const vk::ExtensionProperties& extension) -> std::string_view
+		{
+			return extension.extensionName;
+		});
 
-		std::ranges::sort(extension_names, []
+		std::sort(std::execution::par_unseq, extension_names.begin(), extension_names.end(), []
 		(const std::string_view& a, const std::string_view& b) -> bool
 		{
 			return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());
 		});
 
+		auto extension_present_checker = [&]
+		(const char* required_extension) -> bool
+		{
+			return std::binary_search(extension_names.begin(), extension_names.end(), std::string_view(required_extension));
+		};
+
 		bool all_extensions_supported = true;
-		for (const auto& required_extension : vk_device_extensions)
-			if ( !std::ranges::binary_search( extension_names,
-				std::string_view(required_extension, vk_device_extensions_name_lengthes[&required_extension - &vk_device_extensions[0]]) ) )
-			{
-				return false;
-			}
-		
+		if (!std::all_of(std::execution::par_unseq, vk_device_extensions.begin(), vk_device_extensions.end(), extension_present_checker))
+		{
+			return false;
+		}
+
 		return true;
 	}
 	bool _check_device_fitness_queues(const vk::PhysicalDevice& dev, queue_family_indices& indices)
@@ -278,6 +269,7 @@ class main_app_t
 			throw std::exception("Failed to find suitabe device");
 
 		this->vk_physical_device = std::move(*p_device);
+		this->vk_device_memory_properties = this->vk_physical_device.getMemoryProperties();
 	}
 	void _init_device(const queue_family_indices& indices)
 	{
@@ -321,20 +313,32 @@ class main_app_t
 	void _init_create_command_pool(const queue_family_indices& indices)
 	{
 		vk::CommandPoolCreateInfo create_info{};
-		create_info.queueFamilyIndex = indices.arr[indices.compute_index].value();
 
+		create_info.queueFamilyIndex = indices.arr[indices.compute_index].value();
 		this->vk_compute_command_pool = this->vk_device.createCommandPool(create_info);
+
+		create_info.queueFamilyIndex = indices.arr[indices.transfer_index].value();
+		this->vk_transfer_command_pool = this->vk_device.createCommandPool(create_info);
+
+		create_info.queueFamilyIndex = indices.arr[indices.present_index].value();
+		this->vk_present_command_pool = this->vk_device.createCommandPool(create_info);
 	}
 	void _init_create_command_buffer()
 	{
 		vk::CommandBufferAllocateInfo allocate_info{};
 		allocate_info.commandBufferCount = 1;
+
 		allocate_info.commandPool = this->vk_compute_command_pool;
 		allocate_info.level = vk::CommandBufferLevel::ePrimary;
 		check(this->vk_device.allocateCommandBuffers(&allocate_info, &this->vk_compute_command_buffer));
-	}
-	void _init_compute_pipeline()
-	{
+
+		allocate_info.commandPool = this->vk_transfer_command_pool;
+		allocate_info.level = vk::CommandBufferLevel::ePrimary;
+		check(this->vk_device.allocateCommandBuffers(&allocate_info, &this->vk_transfer_command_buffer));
+
+		allocate_info.commandPool = this->vk_present_command_pool;
+		allocate_info.level = vk::CommandBufferLevel::ePrimary;
+		check(this->vk_device.allocateCommandBuffers(&allocate_info, &this->vk_present_command_buffer));
 	}
 	void _init_create_compute_pipeline()
 	{
@@ -343,11 +347,10 @@ class main_app_t
 		push_consts[0].size = 8;
 		push_consts[0].stageFlags = vk::ShaderStageFlagBits::eCompute;
 
-
 		vk::DescriptorSetLayoutBinding bindings[1]{};
 		bindings[0].stageFlags = vk::ShaderStageFlagBits::eCompute;
 		bindings[0].binding = 0;
-		bindings[0].descriptorType = vk::DescriptorType::eStorageBufferDynamic;
+		bindings[0].descriptorType = vk::DescriptorType::eStorageBuffer;
 		bindings[0].descriptorCount = 1;
 		
 		vk::DescriptorSetLayoutCreateInfo descriptor_set_info{};
@@ -391,8 +394,6 @@ class main_app_t
 		this->load_shader();
 
 		this->_init_create_compute_pipeline();
-
-		this->_init_compute_pipeline();
 	}
 
 	void load_shader()
@@ -420,9 +421,154 @@ class main_app_t
 		create_info.pCode = (uint32_t*)shader_code.data();
 		this->vk_test_shader = this->vk_device.createShaderModule(create_info);
 	}
+
+	
+	vk::DeviceMemory allocate_memory(size_t size, vk::MemoryPropertyFlags mask, vk::MemoryPropertyFlags flags, uint32_t memory_types_mask)
+	{
+		vk::MemoryAllocateInfo alloc_info{};
+		alloc_info.allocationSize = size;
+
+		for (size_t i = 0; i < (size_t)this->vk_device_memory_properties.memoryTypeCount; ++i)
+		{
+			if (memory_types_mask & (1 << i)) {}
+			else continue;
+
+			if ((this->vk_device_memory_properties.memoryTypes[i].propertyFlags & mask) == flags)
+			{
+				alloc_info.memoryTypeIndex = (uint32_t)i;
+				auto memory = this->vk_device.allocateMemory(alloc_info);
+				if (memory)
+					return memory;
+			}
+		}
+
+		return vk::DeviceMemory();
+	}
+	vk::DeviceMemory allocate_local_memory(size_t size, uint32_t memory_types_mask = -1)
+	{
+		return this->allocate_memory(size, 
+			vk::MemoryPropertyFlagBits::eDeviceLocal, 
+			vk::MemoryPropertyFlagBits::eDeviceLocal, 
+			memory_types_mask);
+	}
+	vk::DeviceMemory allocate_global_memory(size_t size, uint32_t memory_types_mask = -1)
+	{
+		return this->allocate_memory(size, 
+			vk::MemoryPropertyFlagBits::eDeviceLocal | vk::MemoryPropertyFlagBits::eHostVisible, 
+			vk::MemoryPropertyFlagBits::eHostVisible, 
+			memory_types_mask);
+	}
+	vk::Buffer allocate_global_buffer(size_t size)
+	{
+		vk::BufferCreateInfo create_info{};
+		create_info.sharingMode = vk::SharingMode::eExclusive;
+		create_info.usage = vk::BufferUsageFlagBits::eStorageBuffer;
+		create_info.size = size;
+		vk::Buffer buffer = this->vk_device.createBuffer(create_info);
+
+		vk::MemoryRequirements reqs = this->vk_device.getBufferMemoryRequirements(buffer);
+		vk::DeviceMemory mem = this->allocate_global_memory(reqs.size, reqs.memoryTypeBits);
+		if (!mem)
+		{
+			this->vk_device.destroyBuffer(buffer);
+			check((bool)mem);
+		}
+
+		this->m_vk_allocations.push_back(mem);
+		this->vk_device.bindBufferMemory(buffer, mem, 0);
+		return buffer;
+	}
 	void payload()
 	{
+		static constexpr size_t n = 16;
+		
+		static constexpr int32_t push[] = { 0, -1 };
 
+		int32_t arr[n];
+
+		void* _device_arr;
+		decltype(arr)* &device_arr = *(decltype(arr)**)&_device_arr;
+
+		
+
+		std::iota(arr, arr + n, 1);
+		vk::CommandBufferBeginInfo begin_info{};
+		vk::FenceCreateInfo fence_create_info{};
+		vk::SubmitInfo submit_info{};
+		submit_info.commandBufferCount = 1;
+
+		//vk::Fence transfer_fence = this->vk_device.createFence(fence_create_info);
+		vk::Fence compute_fence = this->vk_device.createFence(fence_create_info);
+
+
+		vk::Buffer buff = this->allocate_global_buffer(sizeof(arr));
+
+		check(this->vk_device.mapMemory(this->m_vk_allocations[0], 0, sizeof(arr), {}, &_device_arr));
+		memcpy(device_arr, arr, sizeof(arr));
+		this->vk_device.unmapMemory(this->m_vk_allocations[0]);
+		check(this->vk_device.mapMemory(this->m_vk_allocations[0], 0, sizeof(arr), {}, &_device_arr));
+		//memcpy(device_arr, arr, sizeof(arr));
+		this->vk_device.unmapMemory(this->m_vk_allocations[0]);
+
+
+		//submit_info.pCommandBuffers = &this->vk_transfer_command_buffer;
+		//check(this->vk_transfer_queue.submit(1, &submit_info, transfer_fence));
+
+
+
+		vk::DescriptorSet sets[1];
+
+		vk::DescriptorPoolSize pool_size{};
+		pool_size.descriptorCount = 1;
+		pool_size.type = vk::DescriptorType::eStorageBuffer;
+		
+		vk::DescriptorPoolCreateInfo sets_pool_info{};
+		sets_pool_info.maxSets = 1;
+		sets_pool_info.poolSizeCount = 1;
+		sets_pool_info.pPoolSizes = &pool_size;
+
+		auto sets_pool = this->vk_device.createDescriptorPool(sets_pool_info);
+
+		vk::DescriptorSetAllocateInfo sets_allocate_info{};
+		sets_allocate_info.descriptorSetCount = (uint32_t)std::size(sets);
+		sets_allocate_info.pSetLayouts = &this->vk_compute_pipeline_descriptor_set_layout;
+		sets_allocate_info.descriptorPool = sets_pool;
+
+		check(this->vk_device.allocateDescriptorSets(&sets_allocate_info, sets));
+
+		vk::DescriptorBufferInfo buffer_info{};
+		buffer_info.buffer = buff;
+		buffer_info.offset = 0;
+		buffer_info.range = sizeof(arr);
+
+		vk::WriteDescriptorSet buffer_descriptor{};
+		buffer_descriptor.descriptorCount = 1;
+		buffer_descriptor.descriptorType = vk::DescriptorType::eStorageBuffer;
+		buffer_descriptor.dstArrayElement = 0;
+		buffer_descriptor.dstBinding = 0;
+		buffer_descriptor.dstSet = sets[0];
+		buffer_descriptor.pBufferInfo = &buffer_info;
+
+		this->vk_device.updateDescriptorSets(1, &buffer_descriptor, 0, nullptr);
+
+		this->vk_compute_command_buffer.begin(begin_info);
+		this->vk_compute_command_buffer.bindPipeline(vk::PipelineBindPoint::eCompute, this->vk_compute_pipeline);
+		this->vk_compute_command_buffer.bindDescriptorSets(vk::PipelineBindPoint::eCompute, this->vk_compute_pipeline_layout, 0, 1, sets, 0, nullptr);
+		this->vk_compute_command_buffer.pushConstants(this->vk_compute_pipeline_layout, vk::ShaderStageFlagBits::eCompute, 0, sizeof(push), push);
+		this->vk_compute_command_buffer.dispatch(n, 1, 1);
+		this->vk_compute_command_buffer.end();
+
+		submit_info.pCommandBuffers = &this->vk_compute_command_buffer;
+		check(this->vk_compute_queue.submit(1, &submit_info, compute_fence));
+
+
+		check(this->vk_device.waitForFences(1, &compute_fence, VK_FALSE, -1));
+		
+		check(this->vk_device.mapMemory(this->m_vk_allocations[0], 0, sizeof(arr), {}, &_device_arr));
+		this->vk_device.unmapMemory(this->m_vk_allocations[0]);
+
+		this->vk_device.destroyDescriptorPool(sets_pool);
+		this->vk_device.destroyBuffer(buff);
 	}
 public:
 	void run()
@@ -434,7 +580,6 @@ public:
 
 int main()
 {
-
 	main_app_t app;
 
 	try
@@ -444,7 +589,7 @@ int main()
 	}
 	catch (const std::exception& excp)
 	{
-		std::clog << excp.what() << std::endl;
+		fprintf(stderr, "Exception caught:\n%s\n", excp.what());
 		return 1;
 	}
 }
