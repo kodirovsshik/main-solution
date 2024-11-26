8afe35e2f81112d73852659a886b7388429d49dc
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2024-11-27 00:59:41 +0300

    arithmetic coding indev

--- redundancy_tables_builder/main.cpp
+++ redundancy_tables_builder/main.cpp
@@ -1,5 +1,12 @@
 
 #include <stdint.h>
+#include <assert.h>
+
+#include <vector>
+#include <bit>
+#include <random>
+#include <numeric>
+#include <ranges>
 
 using in_t = uint8_t;
 using out_t = uint8_t;
@@ -8,9 +15,14 @@ static constexpr size_t n = 4;
 static constexpr size_t m = 5;
 
 static constexpr size_t table_len = 1 << n;
-using table_t = out_t[table_len];
+static constexpr size_t aux_table_len = 1 << m;
 
-#define bit_distance(a, b) (size_t)std::popcount((a) ^ (b))
+using table_t = out_t[table_len];
+using aux_table_t = out_t[aux_table_len];
+
+std::mt19937_64 global_rng;
+
+#define bit_distance(a, b) (size_t)std::popcount(uintmax_t((a) ^ (b)))
 
 in_t decode(out_t val, const table_t& table)
 {
@@ -28,67 +40,49 @@ in_t decode(out_t val, const table_t& table)
 	return min_idx;
 }
 
-template<class T = size_t>
-class ascending_multiindex
+size_t C(size_t n, size_t k)
 {
-	std::vector<T> idx;
-	T max;
-
-public:
-
-	const T& operator[](size_t n) const noexcept
-	{
-		return idx[n];
-	}
-	size_t 
-	
-	ascending_multiindex(size_t size, T max) noexcept
-		: max(max)
-	{
-		idx.resize(size);
-	}
-	
-	void reset() noexcept
-	{
-		std::ranges::iota(idx, T{});
-	}
-	
-	bool end_reached() const noexcept
-	{
-		return idx.empty() || idx.back() > max;
-	}
-	bool operator++() noexcept
-	{
-		size_t i;
-		for (i = N - 1; i != -1; --i)
-		{
-			if (++idx[i] <= i + max - idx.size() + 1)
-				break;
-		}
-		if (i == -1)
-		{
-			thus->reset();
-			return false;
-		}
-		for (i = i + 1; i != N; ++i)
-		{
-			idx[i] = idx[i - 1] + 1;
-		}
-		return true;
-	}
+	size_t result = 1;
+	for (size_t i = n; i != n - k; --i)
+		result = result * i / (n - i + 1);
+	return result;
 }
 
-std::generator<out_t> enumerate_corruptions(out_t val, size_t len)
+out_t* generate_bitmasks(out_t* p, size_t len, size_t set, out_t val = 0)
 {
-	ascending_multiindex idx(len, m - 1);
-	
-	while (!idx.end_reached())
+	if (set == 0)
 	{
-		
+		*p = val;
+		return p + 1;
 	}
+	if (set == 1)
+	{
+		for (size_t i = 0; i < len; ++i)
+			*p++ = val | (out_t(1) << i);
+		return p;
+	}
+	for (size_t top_bit_pos = set - 1; top_bit_pos < len; ++top_bit_pos)
+	{
+		p = generate_bitmasks(p, top_bit_pos, set - 1, val | (out_t(1) << top_bit_pos));
+	}
+	return p;
+}
+const auto& enumerate_n_bit_masks(size_t set_bits)
+{
+	static std::vector<out_t> masks[m + 1];
+
+	assert(set_bits <= m);
+
+	auto& v = masks[set_bits];
+	if (v.size() != 0)
+		return v;
+
+	v.resize(C(m, set_bits));
+	generate_bitmasks(v.data(), m, set_bits);
+	return v;
 }
 
-size_t eval(const table_t& table)
+size_t evaluate_table(const table_t& table)
 {
 	//evaluation = sum of misdecodings after (upto) *delta* bits are changed
 	static constexpr size_t delta = 1;
@@ -99,15 +93,43 @@ size_t eval(const table_t& table)
 	{
 		for (size_t j = 1; j <= delta; ++j)
 		{
-			for (auto corrupted : enumerate_corruptions(table[i], j))
+			for (auto mask : enumerate_n_bit_masks(j))
 			{
-				result += decode(corrupted, table) != i;
+				result += decode(table[i] ^ mask, table) != i;
 			}
 		}
 	}
+
+	return result;
+}
+
+template<class Rng>
+class unbiased_rng_sampler
+{
+	Rng& rng;
+	size_t bits;
+
+public:
+	unbiased_rng_sampler(Rng& rng)
+		: rng(rng)
+	{
+		const auto val_range = uint64_t(Rng::max() - Rng::min());
+	}
+};
+
+template<class T, class Rng>
+void generate_range_sample(T* out, T min, T max, Rng&& rng = global_rng)
+{
+	unbiased_rng_sampler urng(rng);
+}
+
+void generate_table(table_t& table)
+{
 }
 
 int main()
 {
+	table_t table;
+	generate_table(table);
 
 }
--- temp22/main.cpp
+++ temp22/main.cpp
@@ -2,142 +2,258 @@
 #include <stdint.h>
 #include <assert.h>
 
-#include <vector>
 #include <bit>
 #include <random>
-#include <numeric>
-#include <ranges>
+#include <print>
 
-using in_t = uint8_t;
-using out_t = uint8_t;
+using u64 = uint64_t;
+using u8 = uint8_t;
 
-static constexpr size_t n = 4;
-static constexpr size_t m = 5;
-
-static constexpr size_t table_len = 1 << n;
-static constexpr size_t aux_table_len = 1 << m;
-
-using table_t = out_t[table_len];
-using aux_table_t = out_t[aux_table_len];
-
-std::mt19937_64 global_rng;
-
-#define bit_distance(a, b) (size_t)std::popcount(uintmax_t((a) ^ (b)))
-
-in_t decode(out_t val, const table_t& table)
+u64 bit_stream_prepend(u64 stream, u64 new_vals, u8 vals_size)
 {
-	size_t min_distance = SIZE_MAX;
-	in_t min_idx = 0;
-	for (in_t i = 0; i < (in_t)table_len; ++i)
-	{
-		const size_t current_distance = bit_distance(val, table[i]);
-		if (current_distance < min_distance)
-		{
-			min_distance = current_distance;
-			min_idx = i;
-		}
-	}
-	return min_idx;
+	return (stream << vals_size) | new_vals;
+}
+u64 bit_stream_append(u64 stream, u64 new_vals, u8 stream_size)
+{
+	return stream | (new_vals << stream_size);
 }
 
-size_t C(size_t n, size_t k)
+void reduce_common_powers_of_two(u64& val, u8& max_shift)
 {
-	size_t result = 1;
-	for (size_t i = n; i != n - k; --i)
-		result = result * i / (n - i + 1);
-	return result;
-}
-
-out_t* generate_bitmasks(out_t* p, size_t len, size_t set, out_t val = 0)
-{
-	if (set == 0)
-	{
-		*p = val;
-		return p + 1;
-	}
-	if (set == 1)
-	{
-		for (size_t i = 0; i < len; ++i)
-			*p++ = val | (out_t(1) << i);
-		return p;
-	}
-	for (size_t top_bit_pos = set - 1; top_bit_pos < len; ++top_bit_pos)
-	{
-		p = generate_bitmasks(p, top_bit_pos, set - 1, val | (out_t(1) << top_bit_pos));
-	}
-	return p;
-}
-const auto& enumerate_n_bit_masks(size_t set_bits)
-{
-	static std::vector<out_t> masks[m + 1];
-
-	assert(set_bits <= m);
-
-	auto& v = masks[set_bits];
-	if (v.size() != 0)
-		return v;
-
-	v.resize(C(m, set_bits));
-	generate_bitmasks(v.data(), m, set_bits);
-	return v;
-}
-
-size_t evaluate_table(const table_t& table)
-{
-	//evaluation = sum of misdecodings after (upto) *delta* bits are changed
-	static constexpr size_t delta = 1;
-
-	size_t result = 0;
-
-	for (in_t i = 0; i != (in_t)table_len; ++i)
-	{
-		for (size_t j = 1; j <= delta; ++j)
-		{
-			for (auto mask : enumerate_n_bit_masks(j))
-			{
-				result += decode(table[i] ^ mask, table) != i;
-			}
-		}
-	}
-
-	return result;
+	const u8 shifter = std::min(max_shift, (u8)std::countr_zero(val));
+	val >>= shifter;
+	max_shift -= shifter;
 }
 
 template<class Rng>
-class unbiased_rng_sampler
+constexpr u8 get_rng_entropy(const Rng& rng)
 {
-	Rng& rng;
-	size_t bits;
-
-public:
-	unbiased_rng_sampler(Rng& rng)
-		: rng(rng)
+	if constexpr (std::is_same_v<Rng, std::random_device>)
+		return (u8)rng.entropy();
+	else
 	{
-		const auto val_range = uint64_t(Rng::max() - Rng::min());
+		const auto range = u64(Rng::max() - Rng::min());
+		return std::bit_width(range);
+	}
+}
+
+struct u128
+{
+	u64 x[2];
+
+	static u128 from_u64_multiplication(u64 a, u64 b) noexcept
+	{
+		u128 result;
+		result.x[0] = _umul128(a, b, &result.x[1]);
+		return result;
+	}
+
+	u8 ceil_log2() const noexcept
+	{
+		if (x[1] != 0) [[likely]]
+			return 64 + std::bit_width(x[1] - bool(x[0] == 0));
+
+		if (x[0] != 0) [[likely]]
+			return std::bit_width(x[0] - 1);
+
+		return -1;
+	}
+
+	u8 get_u64_overflow_length() const noexcept
+	{
+		return std::bit_width(x[1]);
+	}
+
+	const u64& operator[](size_t n) const noexcept
+	{
+		return x[n];
+	}
+
+	u128& operator>>=(u8 bits) noexcept
+	{
+		if (bits >= 64) [[unlikely]]
+		{
+			x[0] = x[1] >> (bits - 64);
+			x[1] = 0;
+		}
+		else
+		{
+			x[0] = (x[0] >> bits) | (x[1] << (64 - bits));
+			x[1] >>= bits;
+		}
+		return *this;
+	}
+
+	u128& operator+=(u64 a) noexcept
+	{
+		x[1] += bool(x[0] + a < x[0]);
+		x[0] += a;
+		return *this;
 	}
 };
 
-template<class T, class Rng>
-void generate_range_sample(T* out, T min, T max, Rng&& rng = global_rng)
+struct predetermined_rng
 {
-	unbiased_rng_sampler urng(rng);
-}
+	const char* p;
 
-void generate_table(table_t& table)
+	u64 operator()()
+	{
+		assert(*p == '0' || *p == '1');
+		return *p++ == '1';
+	}
+
+	static u64 min() { return 0; }
+	static u64 max() { return 1; }
+};
+
+template<class Rng>
+struct ac_sampler
 {
-}
+	ac_sampler(Rng& rng)
+		: rng(rng), rng_entropy(get_rng_entropy(rng))
+	{
+	}
 
-#pragma comment(lib, "libksn_window")
-#pragma comment(lib, "libksn_time")
-#pragma comment(lib, "libksn_crc")
-#pragma comment(lib, "libksn_stuff")
-#pragma comment(lib, "zlibstatic")
+private:
+	u64 bit_stream = 0;
+	u64 state = 0; //actual value = stored / 2^64
+	u64 t_num = 1;
+	Rng& rng;
+	u8 t_denom = 0; //t = t_num / 2^t_denom
+	u8 bit_stream_size = 0;
+	const u8 rng_entropy = 0;
 
-#include <mdspan>
+	void retrieve_bits() noexcept
+	{
+		bit_stream = bit_stream_append(bit_stream, rng(), bit_stream_size);
+		bit_stream_size += rng_entropy;
+	}
+
+	u64 get_n_bits(u8 n) noexcept
+	{
+		assert(n <= 64);
+
+		if (n == rng_entropy) [[unlikely]]
+			return (u64)rng();
+
+		u64 result = 0;
+		while (n > bit_stream_size) [[unlikely]]
+		{
+			retrieve_bits();
+			if (bit_stream_size >= n) [[likely]]
+				break;
+			const u8 bits_available = bit_stream_size;
+			result = merge_with_n_existing_bits(result, bits_available);
+			n -= bits_available;
+		}
+		return merge_with_n_existing_bits(result, n);
+	}
+
+	u64 merge_with_n_existing_bits(u64 old_bits, u8 existing_size) noexcept
+	{
+		return bit_stream_prepend(old_bits, extract_existing_n_bits(existing_size), existing_size);
+	}
+	//u64 merge_with_n_existing_bits(u64 old_bits, u8 old_size, u8 existing_size) noexcept
+	//{
+	//	return bit_stream_append(old_bits, extract_existing_n_bits(existing_size), old_size);
+	//}
+
+	u64 extract_existing_n_bits(u8 n) noexcept
+	{
+		assert(n <= bit_stream_size);
+
+		const u64 result = bit_stream & ((1ull << n) - 1);
+		bit_stream >>= n;
+		bit_stream_size -= n;
+		return result;
+	}
+
+	static u8 clamped_subtraction(u8 a, u8 b)
+	{
+		return (a - b) * bool(a >= b);
+	}
+	static u64 safe_add(u64 a, u64 b)
+	{
+		assert(UINT64_MAX - a >= b);
+		return a + b;
+	}
+	static u64 safe_mul(u64 a, u64 b)
+	{
+		assert(a == 0 || UINT64_MAX / a >= b);
+		return a * b;
+	}
+	static u64 safe_lshift(u64 x, u8 s)
+	{
+		assert(std::countl_zero(x) >= s);
+		return x << s;
+	}
+public:
+	//TODO: calling with 0 assumes 2^64
+	u64 operator()(u64 max) noexcept
+	{
+		double e1, e2, e3;
+		e1 = get_stored_entropy();
+
+		do
+		{
+			const u8 unshifted_b = u128::from_u64_multiplication(max, t_num).ceil_log2();
+			if (t_denom >= unshifted_b)
+				break;
+			const u8 b = unshifted_b - t_denom;
+			const u64 bits = get_n_bits(b);
+			t_denom += b;
+
+			u64 _ = safe_mul(bits, t_num);
+			_ = safe_lshift(_, 64 - t_denom);
+			state = safe_add(state, _);
+			//state += bits * t_num << (64 - t_denom); //if the formula was correct, it would be impossible for overflow to occur
+		} while (false);
+		
+		e2 = get_stored_entropy();
+
+		const auto x = u128::from_u64_multiplication(state, max);
+		state = x[0];
+
+		reduce_common_powers_of_two(max, t_denom);
+
+		{
+			auto t = u128::from_u64_multiplication(t_num, max);
+			const u8 overflow_shift = t.get_u64_overflow_length();
+			if (overflow_shift > 0) //TODO: unlikely?
+			{
+				const bool should_round_up = t[0] & ((1ull << overflow_shift) - 1);
+				t >>= overflow_shift;
+				t += should_round_up;
+				t_denom -= overflow_shift;
+			}
+			t_num = t[0];
+		}
+
+		e3 = get_stored_entropy();
+
+		std::println("{:3}: {:.3f} {:.3f} {:.3f}", x[1], e1, e2, e3);
+		
+		return x[1];
+	}
+
+	double get_stored_entropy() const noexcept
+	{
+		return t_denom - log2l((double)t_num);
+	}
+};
 
 int main()
 {
-	table_t table;
-	generate_table(table);
-	
+	predetermined_rng prng{"0111110011000110100110011011011000110101110110101001101111110100"};
+	ac_sampler urng(prng);
+
+	u64 _ = 0;
+	_ = urng(10);
+	_ = urng(10);
+	_ = urng(10);
+	_ = urng(10);
+	_ = urng(10); //breaks here
+	_ = urng(10);
+	_ = urng(10);
+	_ = urng(10);
 }
