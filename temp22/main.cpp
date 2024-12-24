4d9becf494f2a081a8a687a81b2513d643321747
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2024-12-24 16:36:15 +0300

    playing around

--- temp22/main.cpp
+++ temp22/main.cpp
@@ -80,14 +80,58 @@ auto measure()
 	return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1e3;
 }
 
+double binpow(double x, int n)
+{
+	if (n < 0)
+		return 1 / binpow(x, -n);
+
+	double r = 1;
+	while (n)
+	{
+		if (n & 1)
+			r *= x;
+		x *= x;
+		n >>= 1;
+	}
+	return r;
+}
+
+double xpow(double x)
+{
+	const double eps = 0.0001;
+
+	double xint;
+	const double frac = modf(x, &xint);
+
+	double sum = 0;
+	size_t n = 0;
+	double num = 1;
+	double denom = 1;
+
+	while (true)
+	{
+		const double dsum = num / denom;
+		sum += dsum;
+		if (dsum <= eps)
+			break;
+		++n;
+		num *= frac;
+		denom *= n;
+	}
+	return sum * binpow(2.718281828, (int)xint);
+}
+
 int main()
 {
-	value_stream_stats<double> stats;
+	xpow(0);
+	xpow(1);
+	xpow(0.5);
+	//value_stream_stats<double> stats;
 
-	for (size_t i = 0; i < 50; ++i)
-		stats.append(measure());
+	//for (size_t i = 0; i < 50; ++i)
+	//	stats.append(measure());
 
-	std::println("{} +- {} s", stats.mean(), stats.sd());
-	std::println("{} ~ {} s", stats.min, stats.max);
+	//std::println("{} +- {} s", stats.mean(), stats.sd());
+	//std::println("{} ~ {} s", stats.min, stats.max);
 }
 
