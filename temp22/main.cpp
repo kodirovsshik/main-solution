81f1e8715aae25986c46ee85434d81363a760b77
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2025-01-27 19:10:24 +0300

    efficient string-like object handling

--- temp22/main.cpp
+++ temp22/main.cpp
@@ -1,171 +1,48 @@
 
-import <cinttypes>;
-import <climits>;
-import <chrono>;
-import <print>;
-import <filesystem>;
+#include <string_view>
+#include <type_traits>
+#include <utility>
+#include <string>
 
-template<class T>
-T sqr(const T& x)
+template<typename T>
+struct is_const_char_array : std::false_type {};
+
+template<size_t N>
+struct is_const_char_array<const char[N]> : std::true_type {};
+
+template<typename T>
+inline constexpr bool IsCharArray_v = is_const_char_array<T>::value;
+
+template<typename Arg>
+auto process_arg(Arg&& arg)
 {
-	return x * x;
+    if constexpr (IsCharArray_v<std::remove_reference_t<Arg>>)
+    {
+        constexpr size_t array_size = sizeof(Arg) / sizeof(char);
+        return std::string_view(arg, array_size - 1);
+    }
+    else {
+        return std::string_view(std::forward<Arg>(arg));
+    }
 }
 
-template<class T>
-struct value_stream_stats
+template<class... Args> requires(std::is_same_v<Args, std::string_view> && ...)
+void foo2(Args ...ma_strings)
 {
-	T sum{};
-	T sum2{};
-	T min{};
-	T max{};
-	size_t n = 0;
 
-	void append(const T& x)
-	{
-		if (n == 0) [[unlikely]]
-		{
-			min = max = x;
-		}
-		else
-		{
-			if (x < min) min = x;
-			if (x > max) max = x;
-		}
-		sum += x;
-		sum2 += x * x;
-		n++;
-	}
-
-	template<class U = T>
-	U mean() const
-	{
-		return U(sum) / U(n);
-	}
-
-	template<class U = T>
-	U var_p() const
-	{
-		return U(sum2) / U(n) - sqr(U(sum) / U(n));
-	}
-	template<class U = T>
-	U var() const
-	{
-		return U(n) / U(n - 1) * this->var_p<U>();
-	}
-
-	template<class U = T>
-	U sd_p() const
-	{
-		using std::sqrt;
-		return sqrt(this->var_p<U>());
-	}
-	template<class U = T>
-	U sd() const
-	{
-		using std::sqrt;
-		return sqrt(this->var<U>());
-	}
-};
-
-auto measure()
-{
-	auto now = std::chrono::steady_clock::now;
-
-	const auto t1 = now();
-
-	volatile uint32_t i = 0;
-	while (++i != 0);
-
-	const auto t2 = now();
-
-	return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1e3;
 }
 
-double binpow(double x, int n)
+template<std::convertible_to<std::string_view>... Args>
+void foo(Args&&... args) 
 {
-	if (n < 0)
-		return 1 / binpow(x, -n);
-
-	double r = 1;
-	while (n)
-	{
-		if (n & 1)
-			r *= x;
-		x *= x;
-		n >>= 1;
-	}
-	return r;
+    foo2(process_arg(std::forward<Args>(args))...);
 }
 
-double xpow(double x)
-{
-	const double eps = 0.0001;
-
-	double xint;
-	const double frac = modf(x, &xint);
-
-	double sum = 0;
-	size_t n = 0;
-	double num = 1;
-	double denom = 1;
-
-	while (true)
-	{
-		const double dsum = num / denom;
-		sum += dsum;
-		if (fabs(dsum) <= eps)
-			break;
-		++n;
-		num *= frac;
-		denom *= n;
-	}
-	return sum * binpow(2.71828182845904523536, (int)xint);
-}
 
 int main()
 {
-	//xpow(0);
-	//xpow(1);
-	//xpow(0.5);
-	//value_stream_stats<double> stats;
+    const char* a = "a";
+    std::string b = "b";
 
-	//for (size_t i = 0; i < 50; ++i)
-	//	stats.append(measure());
-
-	//std::println("{} +- {} s", stats.mean(), stats.sd());
-	//std::println("{} ~ {} s", stats.min, stats.max);
-
-	using cpath = const std::filesystem::path&;
-
-	cpath root = "D:\\Games\\osu!\\Songs";
-
-	for (auto dir : std::filesystem::directory_iterator(root))
-	{
-		if (!dir.is_directory())
-		{
-			std::println("{} is not a directory", dir.path().string());
-			continue;
-		}
-
-		if (dir.path().filename() == "Failed")
-			continue;
-
-		bool gameplay_file_found = false;
-		for (auto entry : std::filesystem::directory_iterator(dir.path()))
-		{
-			if (entry.is_directory())
-				continue;
-			if (!entry.is_regular_file())
-			{
-				std::println("Unexpected object type for {}", entry.path().string());
-				continue;
-			}
-			gameplay_file_found |= entry.path().string().ends_with(".osu");
-			if (gameplay_file_found)
-				break;
-		}
-		if (!gameplay_file_found)
-			std::println("No gamplay file found in {}", dir.path().string());
-	}
+    foo(a, b, "Hi");
 }
-
